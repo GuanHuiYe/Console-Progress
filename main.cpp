4b7148eb355785b2638ad21d93481d0b34edf5c4
Guan Hui Ye <oscar870524@gmail.com>
2020-02-21 16:02:21 +0800

    修改 : 不符合規範的寫法

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>  // cin cout
-#include <iomanip>   // cout format
-#include <windows.h> // windows API
-#include <string>    // string
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <string>
 
 using namespace std;
 
-// 顯示 / 影藏 Console 光標
+/* 顯示 / 影藏 Console 光標 */
 void ShowConsoleCursor(bool showFlag) {
   HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
   CONSOLE_CURSOR_INFO cursorInfo;
@@ -16,24 +16,22 @@ void ShowConsoleCursor(bool showFlag) {
   }
 }
 
-#define PROGRESSBAR_WIDTH 50
-#define PROGRESSBAR_MAX 100
-#define PROGRESSBAR_MIN 0
+const uint8_t PROGRESSBAR_WIDTH = 50;
+const uint8_t PROGRESSBAR_MAX = 100;
 
 int8_t loaderIndex = 0;
 const string LOADER[] = {"-", "\\", "|", "/"};
 
-void PrintProgress(int value) {
+void PrintProgress(float value) {
   ShowConsoleCursor(false);
-  int progressCount =
-      value / ((PROGRESSBAR_MAX - PROGRESSBAR_MAX) / PROGRESSBAR_WIDTH);
+  int progressCount = value / 2.0;
 
-  // "\r" 能讓光標移到最前面，讓之後 cout 出來的字能覆蓋過去
+  /* "\r" 能讓光標移到最前面，讓之後 cout 出來的字能覆蓋過去 */
   cout << "\r[ Progress ] : [" << setw(progressCount) << setfill('=')
        << ((value != PROGRESSBAR_MAX) ? ">" : "");
   cout << setw(PROGRESSBAR_WIDTH - progressCount) << setfill(' ') << "";
 
-  cout << "] " << setw(2) << setfill(' ') << value << " % ";
+  cout << "] " << setw(2) << setfill(' ') << (int)value << " %";
 
   if (value == PROGRESSBAR_MAX) {
     cout << "Done!" << endl;
@@ -48,7 +46,7 @@ void PrintProgress(int value) {
 }
 
 int main(void) {
-  for (int n = 0; n <= 100; n++) {
+  for (float n = 0; n <= 100; n += 1.5) {
     PrintProgress(n);
     Sleep(100);
   }
